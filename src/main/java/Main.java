6abbcdb72ad663005d4fdd5ded809275f0ae3a59
NJE <h1710101@gmail.com>
2026-08-31 10:28:51 +0800

    Style main application window

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,9 +5,9 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
 import javafx.scene.image.Image;
-
+import javafx.scene.layout.Region;
+import javafx.stage.Stage;
 
 public class Main extends Application {
 
@@ -17,30 +17,57 @@ public class Main extends Application {
     private Button sendButton;
     private Scene scene;
     private Image idleImage = new Image(this.getClass().getResourceAsStream("/images/KawKaw_battle_idle_frame_1.png"));
-    private Image sparedImage = new Image(this.getClass().getResourceAsStream("/images/KawKaw_battle_spared_frame_1.png"));
-    
+    private Image sparedImage = new Image(
+            this.getClass().getResourceAsStream("/images/KawKaw_battle_spared_frame_1.png"));
 
     @Override
     public void start(Stage stage) {
 
-         scrollPane = new ScrollPane();
-         dialogContainer = new VBox();
-         scrollPane.setContent(dialogContainer);
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
 
-         userInput = new TextField();
-         sendButton = new Button("Send");
+        userInput = new TextField();
+        sendButton = new Button("Send");
 
-         DialogBox dialogBox = new DialogBox("Hello!", idleImage);
-         dialogContainer.getChildren().addAll(dialogBox);   
+        DialogBox dialogBox = new DialogBox("Hello!", idleImage);
+        dialogContainer.getChildren().addAll(dialogBox);
 
-         AnchorPane mainLayout = new AnchorPane();
-         mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
 
-         scene = new Scene(mainLayout);
+        scene = new Scene(mainLayout);
 
-         stage.setScene(scene);
-         stage.show();
+        stage.setTitle("Duke");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
 
-         
+        mainLayout.setPrefSize(400.0, 600.0);
+
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+        userInput.setPrefWidth(325.0);
+
+        sendButton.setPrefWidth(55.0);
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setLeftAnchor(userInput, 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
+
+        stage.setScene(scene);
+        stage.show();
+
     }
 }
